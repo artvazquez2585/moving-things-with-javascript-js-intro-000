89c810baadb012a5295832f43cf2f18499e710f9
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-12-02 05:19:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const app = "I don't do much."
 function dodgerLeft(){
-  $("#dodger").on("keyDown",function(e)
-  if(e.which === 37){
-    var left = $("#dodger")[0].style.left.replace('px','');
-    var leftNumbers = parseint(left,10);
-
-  })
+  var left = $("#dodger")[0].style.left.replace('px','');
+  var leftNumbers = parseint(left,10);
+  $(""#dodger"
 }
