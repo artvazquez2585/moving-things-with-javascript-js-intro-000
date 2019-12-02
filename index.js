669b3b341f4800d045dc15e2dec00eede2743c91
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-12-02 05:48:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = "I don't do much."
 //returns dodger position as an integer value
 function getNumericPosition(){
-
+  var currentPosition = $("#dodger")[0].style.left.replace('px','');
+  var currentPositionNum = parseInt(currentPosition,10);
+  return currentPositionNum;  
 }
 //checks for Dodger at boundary, returns true if at boundary
 function dodgerAtBoundary(){
